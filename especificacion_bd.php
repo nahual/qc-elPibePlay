2f6d8d59e6940ef9f501a3ad0e9e544856d9bc2d
tolengo <jplagostena@gmail>
2012-11-10 00:07:58 -0300

    Spec BD

---
+++ especificacion_bd.php
@@ -0,0 +1,18 @@
+<html>
+<head></head>
+<body>
+<pre>
+Base de datos
+
+
+Para conectarse a la base de datos:
+
+Tipo de base de datos: MySQL
+Host: 190.228.29.195
+User: elpibeplay
+Pass: equiequiequi (imaginense al pibe play diciendo “equis equis equis”, pero en la emoción, se le piantan las “s” finales)
+
+http://www.nahual.com.ar/mysql_client/
+</pre>
+</body>
+</html>
